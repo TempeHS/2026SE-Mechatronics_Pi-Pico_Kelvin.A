8a551a259660f37e5c19654ff8feec17492e6671
Kelvin A <126206720+TheFileExplorer@users.noreply.github.com>
2025-08-08 12:42:24 +1000

    Update ultrasonic.py

--- project/py_scripts/ultrasonic.py
+++ project/py_scripts/ultrasonic.py
@@ -1,9 +1,9 @@
 from PiicoDev_Ultrasonic import PiicoDev_Ultrasonic
+from time import sleep_ms
+from PiicoDev_SSD1306 import *
+
+display = create_PiicoDev_SSD1306()
 
 US1 = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
 US2 = PiicoDev_Ultrasonic(id=[1, 0, 0, 0])
 
-class UltraSonic():
-    def __init__(self, US1, US2, debug=False):
-        self.__US1 = US1
-        self.__US2 = US2
