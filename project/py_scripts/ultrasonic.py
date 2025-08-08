a1e563794fcf3597425067c6a491a923431ec7fa
Kelvin A <126206720+TheFileExplorer@users.noreply.github.com>
2025-08-08 10:36:09 +1000

    start US class

--- project/py_scripts/ultrasonic.py
+++ project/py_scripts/ultrasonic.py
@@ -1,4 +1,9 @@
-from lib import PiicoDev_Ultrasonic
+from PiicoDev_Ultrasonic import PiicoDev_Ultrasonic
+
+US1 = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
+US2 = PiicoDev_Ultrasonic(id=[1, 0, 0, 0])
 
 class UltraSonic():
-    def __init__(self, distance):
+    def __init__(self, US1, US2, debug=False):
+        self.__US1 = US1
+        self.__US2 = US2
