1e65c9ddc8c9f6e2ca0f1fd3469682805a5dc663
Kelvin A <126206720+TheFileExplorer@users.noreply.github.com>
2025-08-10 15:55:59 +1000

    UlraSonic Method

--- project/py_scripts/RoboptimusPrime.py
+++ project/py_scripts/RoboptimusPrime.py
@@ -1,23 +1,20 @@
 import time
 from machine import Pin, PWM
 from servo import Servo
+from servomovement import ServoMovement
+from ultrasonic import UltrasonicSensor
 
-
-L_servo_pwm = PWM(Pin(16))
-R_servo_pwm = PWM(Pin(15))
-
-freq = 50
-min_us = 500
-max_us = 2500
-dead_zone_us = 1500
-
-l_servo = Servo(
-    pwm=L_servo_pwm, min_us=min_us, max_us=max_us, dead_zone_us=dead_zone_us, freq=freq
+movement = ServoMovement(
+    forward=(2500, 500),
+    left=(500, 500),
+    right=(2500, 2500),
+    reverse=(500, 2500),
+    stop=(1500, 1500)
 )
-r_servo = Servo(
-    pwm=R_servo_pwm, min_us=min_us, max_us=max_us, dead_zone_us=dead_zone_us, freq=freq
-)
+
+
+
+
+
 
 while True:
-    r_servo.set_duty(500)
-    l_servo.set_duty(2500)
--- project/py_scripts/ultrasonic.py
+++ project/py_scripts/ultrasonic.py
@@ -1,9 +1,25 @@
 from PiicoDev_Ultrasonic import PiicoDev_Ultrasonic
-from time import sleep_ms
-from PiicoDev_SSD1306 import *
 
-display = create_PiicoDev_SSD1306()
+range_Front = PiicoDev_Ultrasonic(id=[1, 0, 0, 0])
+range_Left = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
 
-US1 = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
-US2 = PiicoDev_Ultrasonic(id=[1, 0, 0, 0])
+class Ultrasonic:
+    def __init__(self, threshold=500):
+        self.threshold = threshold
+
+    def stop(self):
+        return range_Front.distance_mm <= self.threshold
+
+    def turn_right(self):
+        return range_Left.distance_mm == self.threshold
+
+    def turn_left(self):
+        return range_Left.distance_mm != self.threshold
+
+
+
+
+
+
+
 
