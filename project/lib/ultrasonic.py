a3684e06e74d9e2a739c758768140f6f27820759
Kelvin A <126206720+TheFileExplorer@users.noreply.github.com>
2025-08-21 17:26:11 +1000

    Ultrasonic class //

--- project/lib/ultrasonic.py
+++ project/lib/ultrasonic.py
@@ -1,11 +1,14 @@
 from PiicoDev_Ultrasonic import PiicoDev_Ultrasonic
 
-range_Front = PiicoDev_Ultrasonic(id=[1, 0, 0, 0])
-range_Left = PiicoDev_Ultrasonic(id=[0, 0, 0, 0])
-
 class Ultrasonic:
-    def __init__(self, threshold=[100, 99, 101]):
-        self.__threshold = [100, 99, 101]
+    def __init__(self, ultrasonic1, ultrasonic2, debug=False):
+        self.__ultrasonic1 = ultrasonic1
+        self.__ultrasonic2 = ultrasonic2
+        self.__debug = debug
 
     def values(self):
-        if range_Front <= 
+        distance1 = self.__ultrasonic1.distance_mm()
+        distance2 = self.__ultrasonic2.distance_mm()
+        if self.__debug:
+            print(distance1, distance2)
+        return (distance1, distance2)
