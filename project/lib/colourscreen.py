a22eb782b6f780cfbfb0e86082991a0a66eaf33c
Kelvin A <126206720+TheFileExplorer@users.noreply.github.com>
2025-08-21 18:17:20 +1000

    Draft controller for Roboptimus

--- project/lib/colourscreen.py
+++ project/lib/colourscreen.py
@@ -1,6 +1,6 @@
 from PiicoDev_VEML6040 import PiicoDev_VEML6040
 
-class ColourSensor:
+class ColourScreen:
     def __init__(self, coloursensor, debug=False):
         self.__coloursensor = coloursensor
         self.__debug = debug
